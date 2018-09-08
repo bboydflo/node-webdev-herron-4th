392ae6f8b25c07834ad38ada058ca0bbb5b7d43f
Florin <florincosmin@fastmail.com>
2018-09-09 00:31:14 +0200

    removed more comments

--- 2. fibonacci/fibotimes.js
+++ 2. fibonacci/fibotimes.js
@@ -1,12 +1,6 @@
 const math = require('./math')
 const util = require('util')
 
-/* for (var num=0; num<45; num++) {
-  let now = new Date().toISOString();
-  // console.log(`${now} Fibonacci for ${num} = ${math.fibonacci(num)}`)
-  console.log(`${now} Fibonacci for ${num} = ${math.fibonacciLoop(num)}`)
-} */
-
 for (var num=0; num<80; num++) {
   // console.log(`${now} Fibonacci for ${num} = ${math.fibonacci(num)}`)
   ((n) => {
